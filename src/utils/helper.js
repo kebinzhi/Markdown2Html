a663891afff130c59dd841aaa4dee79a921bbec7
kebinzhi <251112375@qq.com>
2025-07-05 17:09:39 +0800

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+// 文件路径: src/utils/helper.js
+
 import axios from "axios";
 import MarkdownIt from "markdown-it";
 import markdownItMath from "./markdown-it-math";
@@ -15,7 +17,7 @@ import markdownItImageFlow from "./markdown-it-imageflow";
 import highlightjs from "./langHighlight";
 import markdownItLiReplacer from "./markdown-it-li";
 import markdownUrlAddSpan from "./markdown-it-urlspan";
-import cjkEmphasisFixPlugin from './markdown-it-cjk-emphasis-fix';
+import cjkFriendlyEmphasis from './markdown-it-emphasis-rule.js';
 
 export const axiosGithub = axios.create({
   baseURL: "https://api.github.com",
@@ -82,8 +84,10 @@ export const markdownParserWechat = new MarkdownIt({
   },
 });
 
+// 使用修改后的 emphasis 规则替换默认规则
+markdownParserWechat.inline.ruler.at('emphasis', cjkFriendlyEmphasis);
+
 markdownParserWechat
-  .use(cjkEmphasisFixPlugin)
   .use(markdownUrlAddSpan)
   .use(markdownItSpan) // 在标题标签中添加span
   .use(markdownItTableContainer) // 在表格外部添加容器
@@ -126,8 +130,10 @@ export const markdownParser = new MarkdownIt({
   },
 });
 
+// 使用修改后的 emphasis 规则替换默认规则
+markdownParser.inline.ruler.at('emphasis', cjkFriendlyEmphasis);
+
 markdownParser
-  .use(cjkEmphasisFixPlugin)
   .use(markdownUrlAddSpan)
   .use(markdownItSpan) // 在标题标签中添加span
   .use(markdownItTableContainer) // 在表格外部添加容器
@@ -209,21 +215,6 @@ export const dateFormat = (date, fmt) => {
   }
   return fmt;
 };
-
-// export const url2Blob = (imgUrl) => {
-//   window.URL = window.URL || window.webkitURL;
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("get", imgUrl, true);
-//   xhr.responseType = "blob";
-//   xhr.onload = function () {
-//     if (this.status == 200) {
-//       //得到一个blob对象
-//       var blob = this.response;
-//       console.log("blob", blob)
-//     }
-//   }
-//   xhr.send();
-// }
 
 function getBase64Image(img) {
   var canvas = document.createElement("canvas");
